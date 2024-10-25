549652781776d688a00df18624f8e52b9fe33c2f
Md Abdur Rahman Salman <139158260+ARSalman23@users.noreply.github.com>
2024-10-26 00:28:13 +0600

    Create A. Segment with Small Sum.cpp

---
+++ Step 2/A. Segment with Small Sum.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+const int N = 1e5 + 100;
+int a[N];
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; long long s; cin >> n >> s;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    } 
+    int ans = 0;
+    long long sum = 0;
+    int r = 0;
+    for (int l = 0; l < n; l++)
+    {
+        while(r < n and sum + a[r] <= s)
+        {
+            sum += a[r];
+            r++;
+        }
+        ans = max(ans, r - l);
+        sum -= a[l];
+    }
+    cout << ans << endl;
+    return 0;
+}
